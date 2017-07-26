218250f5334f8d897c8fc8d3d1e8869f97de7e72
Cristian Esquivias <cristian.esquivias@gmail.com>
2017-07-26 16:09:49 -0700

    fish: add local config file

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,11 @@
+#!/usr/bin/env fish
+
 if test -d /opt
-   set -x PATH /opt/bin /opt/local/bin /opt/local/sbin $PATH
+   set -x PATH /opt/local/libexec/gnubin /opt/bin /opt/local/bin /opt/local/sbin $PATH
 end
 set -x PATH ~/.bin $PATH
+
+set local_config
+if test -e $local_config
+   source $local_config
+end
