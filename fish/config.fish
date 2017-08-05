d9b14320fbaf1719284073d4d884845881ae8f07
Cristian Esquivias <cristian.esquivias@gmail.com>
2017-08-05 01:50:21 -0700

    fish: add source highlighting to less calls

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,11 @@ for path in /opt/local/sbin /opt/local/bin /opt/bin ~/.bin
 end
 
 set -x EDITOR emacsclient
+
+# less highlighting
+set -x LESS ' -R '
+if type -q src-hilite-lesspipe.sh
+    set -x LESSOPEN "| src-hilite-lesspipe.sh %s"
+else if [ -f /usr/share/source-highlight/src-hilite-lesspipe.sh ]
+    set -x LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+end
